69ecad773dbe38dcfeed3cca2ba838fa60f0f750
Mohamed Amin Ben Chrifa <mohamed6aminbenchrifa@gmail.com>
2024-11-24 18:58:23 +0100

    implemented basic functionality

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import warnings
+
+if not os.path.exists(".env"):
+    warnings.warn(".env file is missing. Please make sure to create it at the root of the project directory.")
+
+    
+# Load .env file
+load_dotenv()
+
+class Config:
+    # Flask configurations
+    SECRET_KEY = os.getenv("SECRET_KEY")
