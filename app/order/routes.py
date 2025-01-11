48e5b7035741e58dfd5c154985c807861b07125c
metehansalih06 <155652739+metehansalih06@users.noreply.github.com>
2025-01-11 16:09:14 +0100

    recent changes

--- app/order/routes.py
+++ app/order/routes.py
@@ -24,9 +24,9 @@ def checkout():
 
 # Create a route for the order history page
 # The route will return the order_history.html template
-@order_bp.route("/order_history")
+@order_bp.route("/past_orders")
 def order_history():
-    return render_template("history.html")
+    return render_template("past_orders.html")
 
 
 
