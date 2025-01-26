d04749311946454c702879badd8e8b259c192ff8
Fnc-jankos <ayoubtissaoui8@gmail.com>
2025-01-26 17:04:47 +0100

    fix decimal to two places

--- app/static/js/restaurant/dashboard.js
+++ app/static/js/restaurant/dashboard.js
@@ -9,7 +9,7 @@ function showGuthabenModal() {
       })
       .then(data => {
         document.getElementById('totalOrders').innerText = data.total_orders;
-        document.getElementById('totalRevenue').innerText = data.total_revenue + ' EUR';
+        document.getElementById('totalRevenue').innerText = data.total_revenue.toFixed(2) + ' EUR';
         document.getElementById('averageRating').innerText = data.average_rating;
         document.getElementById('guthabenModal').style.display = 'block';
         document.getElementById('overlay').style.display = 'block';
